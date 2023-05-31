4495c134b302d321d4cb3366ebb060d42da650e0
Pierre-Mar <pierre.marjolin@gmail.com>
2023-05-31 18:51:48 +0200

    changes main

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from GESAnalysis.FC.ReaderData import ReaderData
-from GESAnalysis.UI.plot.DistanceMode import DistanceMode
-
 def run():
-    r = ReaderData()
-    d = r.read_file("/home/pierre/Documents/python/files_gesanalyser/files/2019_missions.txt")
-    
-    dist = DistanceMode([(d, "2019")])
-    dist = DistanceMode([(d, "2019"), (d, '2020')])
-    dist = DistanceMode([(d, "2019"), (d, '2020'), (d, '2021')])
-    dist = DistanceMode([(d, "2019"), (d, '2020'), (d, '2021'), (d, '2022')])
+    print("HelloWorld")
+
 
 if __name__ == "__main__":
     # Print a simple "Hello World !"
