a5d11344ed56d248a96c09d0323a084b27fdeee6
Pierre-Mar <pierre.marjolin@gmail.com>
2023-05-31 15:36:27 +0200

    Placement des étiquettes est espacement barres

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ def run():
     r = ReaderData()
     d = r.read_file("/home/pierre/Documents/python/files_gesanalyser/files/2019_missions.txt")
     
+    dist = DistanceMode([(d, "2019")])
     dist = DistanceMode([(d, "2019"), (d, '2020')])
-    
+    dist = DistanceMode([(d, "2019"), (d, '2020'), (d, '2021')])
+    dist = DistanceMode([(d, "2019"), (d, '2020'), (d, '2021'), (d, '2022')])
 
 if __name__ == "__main__":
     # Print a simple "Hello World !"
